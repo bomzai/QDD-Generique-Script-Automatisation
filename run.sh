2c714a01d7d83582c77a5e5a9208d0863a2d4682
AdilLazar19 <adil.lazar@bomzai.fr>
2025-12-31 14:21:09 +0100

    0.0.1 fixed run.sh soda scan

--- run.sh
+++ run.sh
@@ -14,7 +14,7 @@ echo "3/4 Vérification de la connexion Snowflake..."
 python scripts/3_generate_soda_config.py ./soda/configs/dev-config.yml
 
 echo "Lancement du Scan Soda..."
-soda scan -d snowflake -c ./soda/configs/dev-config.yml -s target/soda_checks/*.yml -srf target/results.json || true
+soda scan -d snowflake -c ./soda/configs/dev-config.yml target/soda_checks/*.yml -srf target/results.json || true
 
 echo "4/4 Génération de la configuration Soda..."
 python scripts/4_push_results_to_snowflake.py ./target/results.json
